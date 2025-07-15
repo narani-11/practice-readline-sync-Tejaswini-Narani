674ebc8d7bcc3b416b0da72711f2ef75065ee4dd
narani-11 <naranitejaswini29@gmail.com>
2025-07-15 00:40:52 -0500

    Update quiz.js

--- quiz.js
+++ quiz.js
@@ -15,3 +15,20 @@ console.log(`2) ${answer2}`);
 console.log(`3) ${answer3}`);
 console.log(`4) ${answer4}`);
 console.log(`5) ${answer5}`);
+
+Output are:
+What is your name? John
+Hello, John! Let's start the quiz.
+1) What data type stores true or false? boolean
+2) What operator is used to assign a value? =
+3) Enter a number: 42
+4) What keyword declares a variable in JavaScript? let
+5) How do you write a single line comment? // comment
+
+Thanks for your answers:
+1) boolean
+2) =
+3) 42
+4) let
+5) // comment
+
